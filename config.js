97ddf1513552d760008a363fc90696fcd88f774d
Aayush Kumar <99812709+Aayush-Kr-13@users.noreply.github.com>
2024-08-24 18:03:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const sessionSecret="mysitesessionsecret"
 // const emailUser="flavio7@ethereal.email"
 const emailUser="anusinghbisen15@gmail.com"
 // const  emailpassword="BG61pmqPRcF3z2vkVD"
-const  emailpassword="vxlqmstyjiphybbc"
+const  emailpassword="<Password>"
 
 module.exports={
     sessionSecret,
